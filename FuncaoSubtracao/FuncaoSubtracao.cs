90e4979f7de0cfad58184b5e58b305b2301aa4e1
Juarez Geraldo Silva Jr <juarez.junior@grupomult.com.br>
2022-10-25 21:45:29 -0300

    primeiro commit

---
+++ FuncaoSubtracao/FuncaoSubtracao.cs
@@ -0,0 +1,36 @@
+﻿namespace FuncaoSubtracao
+{
+    public class FuncaoSubtracao
+    {
+        public void subtracaoNumeros()
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("Aplicação para subtrair dois números");
+            Console.WriteLine("====================================");
+
+            Console.WriteLine("Informe o primeiro número");
+
+            while (!int.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("Valor informado está inválido");
+            };
+
+
+            Console.WriteLine("Informe o segundo número");
+
+            while (!int.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("Valor informado está inválido");
+            };
+
+            int subtracao = num1 - num2;
+
+            Console.WriteLine("A subtração dos números é: " + subtracao);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ infnet-bl6-fdC-tp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using FuncaoSoma;
+using FuncaoSubtracao;
+using FuncaoMultiplicacao;
+
+namespace AplicacaoPrincipal
+
+{
+    public class Programa
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+
+            while (opcao != 9)
+            {
+                Console.WriteLine("Informe qual operação quer realizar:");
+                Console.WriteLine("1 - Somar");
+                Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("3 - Multiplicar");
+                Console.WriteLine("4 - Dividir");
+                Console.WriteLine();
+                Console.WriteLine("9 - Finalizar");
+
+                while (!int.TryParse(Console.ReadLine(), out opcao))
+                {
+                    Console.WriteLine("Valor informado está inválido");
+                };
+
+                Console.Clear();
+
+                switch (opcao)
+                {
+                    case 1:
+                        FuncaoSoma.FuncaoSoma soma = new FuncaoSoma.FuncaoSoma();
+                        soma.somaNumeros();
+                        break;
+                    case 2:
+                        FuncaoSubtracao.FuncaoSubtracao subtrai = new FuncaoSubtracao.FuncaoSubtracao();
+                        subtrai.subtracaoNumeros();
+                        break;
+                    case 3:
+                        FuncaoMultiplicacao.FuncaoMultiplicacao multiplica = new FuncaoMultiplicacao.FuncaoMultiplicacao();
+                        multiplica.multiplicaNumeros();
+                        break;
+                    case 4:
+                        FuncaoDivisao.FuncaoDivisao divide = new FuncaoDivisao.FuncaoDivisao();
+                        divide.divideNumeros();
+                        break;
+                }
+                Console.Clear();
+            }
+
+        }
+    }
+}
